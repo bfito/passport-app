3845c7cab5642a71b32dfe0973094285ac017722
JPZ <jeanpaulzune@gmail.com>
2017-03-03 09:27:35 -0500

    Commiting with public/uploads gitignore

--- routes/rooms-routes.js
+++ routes/rooms-routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const ensure = require('connect-ensure-login');
+const multer = require('multer');
 
 const Room = require('../models/room-model');
 
 const roomsRoutes = express.Router();
+const uploads = multer({ dest: '__dirname' + '/../public/uploads/' });
 
 roomsRoutes.get('/rooms/index', ensure.ensureLoggedIn(), (req, res, next) => {
   Room.find({owner: req.user._id}, (err, myRooms) => {
@@ -20,13 +22,20 @@ roomsRoutes.get('/rooms/new', ensure.ensureLoggedIn(), (req, res, next) => {
 });
 
 // router.post('/rooms', ensureAuthenticated, (req, res, next) => {
-roomsRoutes.post('/rooms', (req, res, next) => {
-  const newRoom = new Room ({
-    name:  req.body.name,
-    desc:  req.body.desc,
-    picture: "https://media.giphy.com/media/shGbMNwth7X9K/giphy.gif",
-    owner: req.user._id   // <-- we add the user ID
-  });
+roomsRoutes.post('/rooms',
+  ensure.ensureLoggedIn(),
+  // ('picture') refers to name="picture" in the form
+  uploads.single('picture'),
+
+   (req, res, next) => {
+     const filename = req.file.filename;
+
+    const newRoom = new Room ({
+      name:  req.body.name,
+      desc:  req.body.desc,
+      picture: `/uploads/${filename}`,
+      owner: req.user._id   // <-- we add the user ID
+    });
 
   newRoom.save ((err) => {
     if (err) {
